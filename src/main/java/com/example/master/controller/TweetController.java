ec7b79ff7a2dc73a0c86ac0d8ca11638d9c69734
JakubKowalski <kubabartek1@op.pl>
2018-04-07 16:45:07 +0200

    Added page to search Twitter

--- src/main/java/com/example/master/controller/TweetController.java
+++ src/main/java/com/example/master/controller/TweetController.java
@@ -1,6 +1,5 @@
 package com.example.master.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
@@ -8,11 +7,12 @@ import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.naming.directory.SearchResult;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class TweetController {
@@ -20,12 +20,24 @@ public class TweetController {
     private Twitter twitter = new TwitterTemplate("jJ1KSqAu0i5Ke27E4WSE6pXiT","sAQkzKrRji7pmcYAdvmCCR7L3IstOVzaffllTEcLQ1gWqTIRiQ");
 
     @RequestMapping("/")
-    public String hello(@RequestParam(defaultValue = "Tajniki SpringMVC4") String search, Model model){
+    public String home(){
+        return "searchPage";
+    }
+
+    @RequestMapping("/result")
+    public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model){
         SearchResults results = twitter.searchOperations().search(search);
         List<Tweet> tweets = results.getTweets();
         model.addAttribute("search", search);
         model.addAttribute("tweets", tweets);
         return "resultPage";
+    }
+
+    @RequestMapping(value = "postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String search = request.getParameter("search");
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
     }
 
 }
